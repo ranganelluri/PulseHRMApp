982328f0d5dcf306f811b049a8d6b8358d0ddcf6
ranganelluri <ranganelluri@users.noreply.github.com>
2018-11-05 18:55:07 +0530

    Initial commit

---
+++ app/src/main/java/com/pulsehrm/pulsehrmapp/Beans/LoginAPI.kt
@@ -0,0 +1,20 @@
+package com.pulsehrm.pulsehrmapp.Beans
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginAPI(@SerializedName("Message")
+                    val message: String = "",
+                    @SerializedName("Schema")
+                    val schema: String = "",
+                    @SerializedName("Email ID")
+                    val emailID: String = "",
+                    @SerializedName("Employee Number")
+                    val employeeNumber: String = "",
+                    @SerializedName("AID LOGIN")
+                    val aidLogin: Int = 0,
+                    @SerializedName("Company Code")
+                    val companyCode: String = "",
+                    @SerializedName("Flag")
+                    val flag: Int = 0,
+                    @SerializedName("Punch Flag")
+                    val punchFlag: Int = 0)
